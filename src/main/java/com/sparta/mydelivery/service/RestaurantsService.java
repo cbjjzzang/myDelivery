4c42251ea74184441a822677eac6a59b74da2e81
Bongjin Choi <cbjjzzang@naver.com>
2022-04-06 10:36:34 +0900

    거리에 따라 배달 가능지역, 추가배달비 계산

--- src/main/java/com/sparta/mydelivery/service/RestaurantsService.java
+++ src/main/java/com/sparta/mydelivery/service/RestaurantsService.java
@@ -16,10 +16,7 @@ public class RestaurantsService {
     private final RestaurantsRepository restaurantsRepository;
 
     public Restaurants registerRestaurants(RestaurantsDto restaurantsDto) {
-        if(restaurantsDto.getMinOrderPrice() < 1000 || restaurantsDto.getMinOrderPrice() > 100000) throw new IllegalArgumentException("주문금액은 1000원 이상 100000원 미만입니다.");
-        if(restaurantsDto.getMinOrderPrice() % 100 != 0) throw new IllegalArgumentException("100원 단위만 입력해주세요.");
-        if(restaurantsDto.getDeliveryFee() < 0 || restaurantsDto.getDeliveryFee() > 10000) throw new IllegalArgumentException("배달비는 10000원 이하 입니다.");
-        if(restaurantsDto.getDeliveryFee() % 500 != 0) throw new IllegalArgumentException("배달비는 500원 단위로 입력해주세요.");
+        restaurantValid(restaurantsDto);
         Restaurants restaurants = new Restaurants(restaurantsDto);
         return restaurantsRepository.save(restaurants);
     }
@@ -39,5 +36,12 @@ public class RestaurantsService {
             }
         }
             return availableRestaurants;
+    }
+
+    private void restaurantValid(RestaurantsDto restaurantsDto) {
+        if(restaurantsDto.getMinOrderPrice() < 1000 || restaurantsDto.getMinOrderPrice() > 100000) throw new IllegalArgumentException("주문금액은 1000원 이상 100000원 미만입니다.");
+        if(restaurantsDto.getMinOrderPrice() % 100 != 0) throw new IllegalArgumentException("100원 단위만 입력해주세요.");
+        if(restaurantsDto.getDeliveryFee() < 0 || restaurantsDto.getDeliveryFee() > 10000) throw new IllegalArgumentException("배달비는 10000원 이하 입니다.");
+        if(restaurantsDto.getDeliveryFee() % 500 != 0) throw new IllegalArgumentException("배달비는 500원 단위로 입력해주세요.");
     }
 }
